d0fee43c8fae71c569d3eb8a29fbe7336deb76db
brychanmd <bmorrisdafydd@gmail.com>
2020-10-03 13:55:52 +0100

    tidy up main.js

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 "use strict";
 
+
+// Game Board Module
 const gameboard = (() => {
 
-// Players
+	var cells = document.querySelectorAll('.cell');
+	cells.forEach(element => element.addEventListener('click', () => {
+		console.log(element.id);
+		element.style.backgroundImage = "url(/assets/o.png)";
+	}));
 
+})();
+
+// Player Factory
 const Player = (name, symbol) => {
 
 };
-
-// Game Board
-
-var cells = document.querySelectorAll('.cell');
-cells.forEach(element => element.addEventListener('click', () => {
-	console.log(element.id);
-	element.style()
-}));
-
-
-
-})();
