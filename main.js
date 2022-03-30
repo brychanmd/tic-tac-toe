6bd8f9469015bc0e01672faf85fdbc8d1bbd81f1
brychanmd <bmorrisdafydd@gmail.com>
2022-03-30 23:44:05 +0100

    tic tac toe unbeatable AI finished

--- main.js
+++ main.js
@@ -40,6 +40,7 @@ const displayController = (() => {
 			value.classList.remove('circle', 'cross'); // Remove tokens from the board.
 			gameBoard.board[value.id] = ''; // Replace board array with all empty values.
 			gameBoard.container.classList.remove('disabled');
+            value.removeEventListener('click', () => {});
 
 		});
 
@@ -52,11 +53,6 @@ const displayController = (() => {
 		gameBoard.board.splice(index, 1, player.symbol);
 		bleep.currentTime = 0
 		bleep.play();
-		
-		player.cells.push( parseInt(index) );
-		player.cells.sort();
-
-        // Also remove event listener?
 	}
 
 	const displayResults = ( roundWinner ) => {
@@ -86,7 +82,10 @@ const gameLogic = (() => {
 	
 	var startingPlayer = player1;
 	var activePlayer = startingPlayer;
-	let moves = 0;
+
+    // For use by the minimax function.
+    var nodesMap = new Map();
+    var maxDepth = -1;
 
     const winningCells = [
 		[0, 1, 2],
@@ -114,10 +113,11 @@ const gameLogic = (() => {
 
     const startGame = () => {
 
-        gameBoard.container.style.display = 'flex';        
+        gameBoard.container.style.display = 'flex';
 
         if ( onePlayerMode ) {
 
+            console.log( gameBoard.board );
             if ( startingPlayer === player2 ) {
                 aiMove();
             }
@@ -147,78 +147,201 @@ const gameLogic = (() => {
 
     const playerMove = ( cell ) => {
 
+        console.log(activePlayer);
+
         // Place Marker.
         displayController.placeMarker(cell, activePlayer);
 
+        let result = checkResults( gameBoard.board );
+
         // Check results after current move.
-        if ( checkResults( gameBoard.board, activePlayer ) ) {
+        if ( activePlayer.symbol === result ) {
             // Winner!
             displayController.displayResults( activePlayer );
-       } else if (9 >= moves) {
+       } else if ( 'draw' === result ) {
             // Draw :/
             displayController.displayResults( null )
        } else {
-            // Switch active player and move on..
-            moves++;
-            activePlayer == player1 ? activePlayer = player2 : activePlayer = player1;
-            onePlayerMode ? aiMove() : ''; // Trigger AI move if it's single player mode.
+            setTimeout(() => {
+                // Switch active player and move on..
+                activePlayer == player1 ? activePlayer = player2 : activePlayer = player1;
+                onePlayerMode ? aiMove() : ''; // Trigger AI move if it's single player mode.
+            }, 800);
        }
     }
 
     const aiMove = () => {
         
         // Find best move, make move.
-        let bestMove = findBestMove( gameBoard.board, player2, 0 );
+        let bestMove = findBestMove( gameBoard.board );
         displayController.placeMarker( bestMove, player2 );
 
+        let result = checkResults( gameBoard.board );
+
         // Check results after move.
-        if ( checkResults( gameBoard.board, player2 ) ) {
+        if ( player2.symbol === result ) {
             // Winner!
             displayController.displayResults( player2 );
-        } else if (9 >= moves) {
+        } else if ( 'draw' === result ) {
             // Draw :/
             displayController.displayResults( null )
         } else {
             // Move on..
-            moves++;
             activePlayer = player1;
         }
     }
 
     // Uses the minimax algorithm to find the best move.
-    const findBestMove = ( gameBoard, player, depth ) => {
-        
-
-        let currentBoardState = [...gameBoard];
-
-        
-
-        
+    const findBestMove = ( gameBoard ) => {
+        return miniMax(gameBoard);
     }
 
-	const checkResults = ( boardstate, currentPlayer ) => {
-		
-        let check = winningCells.some( wc => { 
-            return wc.every( val => boardstate[val] === currentPlayer.symbol );
-		} );
+    const miniMax = (
+        gameBoard,
+        maximising = true, 
+        callback = () => {},
+        depth = 0, 
+    ) => {
+        
 
-        return check;
+        // Clear nodesMap if the function is called for a new move
+        if(depth == 0) nodesMap.clear();
+
+        // If the board state is a terminal one, return the heuristic value
+        if ( checkResults( gameBoard ) || depth === maxDepth ) {
+            if( checkResults( gameBoard ) === 'circle') {
+                return 100 - depth;
+            } else if ( checkResults( gameBoard ) === 'cross') {
+                return -100 + depth;
+            }
+            return 0;
+        }
+
+        if ( maximising ) {
+            let best = -100;
+            let availableMoves = [];
+            gameBoard.forEach( (val, index) => val === '' ? availableMoves.push(index) : '');
+            availableMoves.forEach( index => {
+                
+                // Initialize new board with copy of current state.
+                const child = [...gameBoard];
+                // Create a child node by inserting the maximizing symbol 'circle' into the current empty cell
+                child[index] = 'circle';
+                //Recursively calling miniMax this time with the new board and minimizing turn and incrementing the depth
+                const nodeValue = miniMax(child, false, callback, depth + 1);
+                //Updating best value
+                best = Math.max(best, nodeValue);
+                
+                //If it's the main function call, not a recursive one, map each heuristic value with it's moves indices
+                if ( depth == 0 ) {
+                    // Comma separated indices if multiple moves have the same heuristic value
+                    const moves = nodesMap.has(nodeValue)
+                        ? `${nodesMap.get(nodeValue)},${index}`
+                        : index;
+                    nodesMap.set(nodeValue, moves);
+                }
+
+            });
+            
+            //If it's the main call, return the index of the best move or a random index if multiple indices have the same value
+            if ( depth == 0 ) {
+                if (typeof nodesMap.get(best) == "string") {
+                    const arr = nodesMap.get(best).split(",");
+                    const rand = Math.floor(Math.random() * arr.length);
+                    var myret = arr[rand];
+                } else {
+                    var myret = nodesMap.get(best);
+                }
+                //run a callback after calculation and return the index
+                callback( myret );
+                return myret;
+            }
+            //If not main call (recursive) return the heuristic value for next calculation
+            return best;
+
+        }
+
+        if ( ! maximising ) {
+            let best = 100;
+            let availableMoves = [];
+            gameBoard.forEach( (val, index) => val === '' ? availableMoves.push(index) : '');
+            availableMoves.forEach( index => {
+                
+                // Initialize new board with copy of current state.
+                const child = [...gameBoard];
+                // Create a child node by inserting the maximizing symbol 'cross' into the current empty cell
+                child[index] = 'cross';
+                //Recursively calling miniMax this time with the new board and minimizing turn and incrementing the depth
+                const nodeValue = miniMax(child, true, callback, depth + 1);
+                //Updating best value
+                best = Math.min(best, nodeValue);
+                
+                //If it's the main function call, not a recursive one, map each heuristic value with it's moves indices
+                if ( depth == 0 ) {
+                    // Comma separated indices if multiple moves have the same heuristic value
+                    const moves = nodesMap.has(nodeValue)
+                        ? `${nodesMap.get(nodeValue)},${index}`
+                        : index;
+                    nodesMap.set(nodeValue, moves);
+                }
+
+            });
+            
+            //If it's the main call, return the index of the best move or a random index if multiple indices have the same value
+            if ( depth == 0 ) {
+                if ( typeof nodesMap.get(best) == "string" ) {
+                    const arr = nodesMap.get(best).split(",");
+                    const rand = Math.floor( Math.random() * arr.length );
+                    var myret = arr[rand];
+                } else {
+                    var myret = nodesMap.get(best);
+                }
+                //run a callback after calculation and return the index
+                callback( myret );
+                return myret;
+            }
+            //If not main call (recursive) return the heuristic value for next calculation
+            return best;
+
+        }
+
+    }
+
+	const checkResults = ( boardstate ) => {
+		
+        // X wins.
+        let checkCross = winningCells.some( wc => { 
+            return wc.every( val => boardstate[val] === 'cross' );
+		} );
+        if ( checkCross ) return 'cross';
+
+        // O wins.
+        let checkCircle = winningCells.some( wc => { 
+            return wc.every( val => boardstate[val] === 'circle' );
+		} );
+        if ( checkCircle ) return 'circle';
+
+        // It's a tie.
+        let drawCheck = boardstate.every( val => val !== '' );
+        if ( drawCheck ) return 'draw';
+
+        // Game is not in a terminal state.
+        return null;
 
 	}
 
 	const resetGame = () => {
-        moves = 0;
-		player1.cells = [];
-		player2.cells = [];
+        startingPlayer == player1 ? startingPlayer = player2 : startingPlayer = player1; //  Take turns starting new game.
 		displayController.resetBoard();
 		activePlayer = startingPlayer;
+        startGame();
+
 	}
 
 	gameBoard.resetBtn.addEventListener('click', () => {
 		resetGame();
 	}); // Event listener for the reset button.
 	gameBoard.replayBtn.addEventListener('click', () => {
-		startingPlayer === player1 ? startingPlayer = player2 : startingPlayer = player1; //  Take turns starting new game.
 		resetGame();
 	}); // Event listener for the reset button.
 
